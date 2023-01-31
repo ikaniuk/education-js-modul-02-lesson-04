3613ecb60cb59d6a07358ab58d1aeb1f6ae4e3bc
Ihor Kaniuk <ikanyuk@icloud.com>
2023-01-31 18:02:21 +0200

    add-task-03

---
+++ js/js-instructor-examples.js
@@ -0,0 +1,44 @@
+/*
+ * 1) Напиши функцию calculateTotalPrice(items)
+ * которая принимает массив цен (чисел) и возвращает их сумму
+ */
+
+
+// function calculateTotalPrice(items) {
+    
+//     let total = 0;
+    
+//     for (const value of items) {
+//         total += value;
+//     }
+    
+//     return total;
+// }
+
+// const r1 = calculateTotalPrice([2, 2, 2]);
+// console.log(r1);
+// console.log(calculateTotalPrice([2, 3, 4,]));
+// console.log(calculateTotalPrice([100, 200, 50]));
+
+
+// 2)  Напиши функцию logItems(items) для перебора и логирования массива
+
+// function logItems(items) {
+//     for (const item of items) {
+//         console.log(item);
+//     }
+// }
+
+// logItems([2, 3, 1]);
+// logItems(['Polly', 'Kiwi', 'Ajyax', 'Logo']);
+
+
+// 3)Напиши функцию findLogin(allLogins, login) для поиска логина
+//  * - Если логина нет, вывести сообщение 'Пользователь [логин] не найден.'
+//  * - Если нашли логин, вывести сообщение 'Пользователь [логин] найден.'
+
+// function findLogin(allLogins, loginToFind) {
+    
+// }
+
+
