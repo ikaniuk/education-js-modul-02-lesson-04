db74f2179cdec17d390d486a78fd9d84df389adf
Ihor Kaniuk <ikanyuk@icloud.com>
2023-02-25 15:42:13 +0200

    complete-task-05

--- js/other-task.js
+++ js/other-task.js
@@ -24,11 +24,39 @@
 //         if (!isNaN(value)) {
 //             sum += value;
 //             count += 1;
-//         }       
-//     }    
+//         }
+//     }
 //     return sum / count;
 // }
 
 // console.log(avg(1, 2, 3))
 // console.log(avg(4, 5, 'fg'))
 // console.log(avg('asd', 'a', 1, 1));
+
+
+
+// ------------------------Task-05------------------------
+// // Задание:
+// // Создайте функцию, которая принимает массив значений и возвращает массив с
+// // дублирующимися значениями или null если повторяющихся значений в массиве нет.
+
+// function serchDuplicateNumbers(numbers) {
+//     const duplicateNumbers = [];
+//     for (let i = 0; i < numbers.length; i += 1){
+//         for (let j = i + 1; j < numbers.length; j += 1){
+//             if (numbers[i] === numbers[j]) {
+//                 duplicateNumbers.push(numbers[i]);
+//             }
+//         }
+//     }
+//     if (duplicateNumbers.length > 0) {
+//         return duplicateNumbers;
+//     } else {
+//         return null;
+//     }
+    
+// }
+
+// console.log(serchDuplicateNumbers([1, 2, 4, 1, 2]));
+// console.log(serchDuplicateNumbers([1000, 20, 433, 1, 20]));
+// console.log(serchDuplicateNumbers([1000, 20, 433, 1, 200]));
